b85db51a274c4ad9f3dd6308b054f6872a62cd1d
Mubbasher Ahmed Qureshi <mubbi_qureshi@hotmail.com>
2025-07-04 17:59:39 +0500

    prevent prod db destruction

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +22,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::shouldBeStrict(! $this->app->isProduction());
+        DB::prohibitDestructiveCommands($this->app->isProduction());
     }
 }
