76a803ab8a04133df66853bee05292c68dbcce64
Mubbasher Ahmed Qureshi <mubbi_qureshi@hotmail.com>
2025-07-04 18:00:41 +0500

    lint run

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
