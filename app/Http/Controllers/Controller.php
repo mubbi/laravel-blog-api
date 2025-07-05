85ef648efbc95f993bac7a04fb1923cf4aee9451
mubbasher-ahmed <mubbi_qureshi@hotmail.com>
2025-07-05 15:28:03 +0500

    [main] make base controller stricter

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,7 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
 
+/**
+ * Base controller for all application controllers.
+ *
+ * @internal
+ */
 abstract class Controller
 {
     //
