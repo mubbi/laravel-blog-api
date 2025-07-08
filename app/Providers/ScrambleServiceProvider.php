6977802326d1ea663e411e9d1411b3b7f7a72c44
Mubbasher Ahmed Qureshi <mubbi_qureshi@hotmail.com>
2025-07-08 15:26:42 +0500

    [main] add scramble provider

---
+++ app/Providers/ScrambleServiceProvider.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Providers;
+
+use App\Enums\UserRole;
+use App\Models\User;
+use Dedoc\Scramble\Scramble;
+use Dedoc\Scramble\Support\Generator\OpenApi;
+use Dedoc\Scramble\Support\Generator\SecurityScheme;
+use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
+
+class ScrambleServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        Gate::define('viewApiDocs', function (User $user) {
+            return app()->isLocal() || $user->hasRole(UserRole::ADMINISTRATOR->value);
+        });
+
+        /** @phpstan-ignore-next-line method.nonObject */
+        Scramble::configure()
+            ->routes(function (Route $route): bool {
+                return Str::startsWith($route->uri, 'api/');
+            })
+            ->withDocumentTransformers(function (OpenApi $openApi): void {
+                /** @phpstan-ignore-next-line argument.type */
+                $openApi->secure(
+                    /** @phpstan-ignore-next-line argument.type */
+                    SecurityScheme::http('bearer')
+                );
+            });
+    }
+}
